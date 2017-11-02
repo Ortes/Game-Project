c429eddb197b954b9966b4ef99c2595f6946e687
Onyldas <onyldas@gmail.com>
2017-11-02 18:28:40 +0100

    Add animations on Player (idle/walk)

---
+++ Assets/Scripts/Player_Scripts/Player_AnimScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_AnimScript : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            anim.CrossFade("walk", 0.1f);
+        }
+            if (Input.GetKey(KeyCode.Z))
+        {
+            anim.SetFloat("isWalking", 1);
+            anim.SetFloat("isIdle", 0);
+        }
+        else
+        {
+            anim.SetFloat("isWalking", 0);
+            anim.SetFloat("isIdle", 1);
+        }
+	}
+}
