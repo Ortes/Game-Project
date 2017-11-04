a706fb40419c1124da4585cd02b418d450fa9b20
Ortes <malo.allee@gmail.com>
2017-11-04 03:23:32 +0100

    player move and rotate

--- Assets/Scripts/Player_Scripts/Player_AnimScript.cs
+++ Assets/Scripts/Player_Scripts/Player_AnimScript.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class Player_AnimScript : MonoBehaviour {
 
-    public float speed = 2f;
+    public float speed = 2.5f;
 
     private Animator anim;
-    private Rigidbody rb;
 
 	void Start ()
     {
         anim = GetComponent <Animator> ();
-        rb = GetComponent <Rigidbody> ();
         anim.speed = speed;
     }
 
     // Update is called once per frame
     void Update ()
     {
-        float vaxe = Input.GetAxis("Vertical") * speed;
-        if (vaxe != 0f)
+        if (Input.GetAxis("Vertical") != 0f)
         {
             anim.SetBool("IsWalking", true);
-            Vector3 newPos = rb.position + new Vector3(0, 0, vaxe) * Time.deltaTime;
-            rb.MovePosition(newPos);
+            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
         }
         else
             anim.SetBool("IsWalking", false);
-
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * speed);
 	}
 }
