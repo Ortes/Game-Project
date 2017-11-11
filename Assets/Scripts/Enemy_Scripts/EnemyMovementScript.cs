f758dbec413102223bbca8d999e9ced1f900b91c
Brasigen <vincent_william@live.fr>
2017-11-11 17:14:21 +0000

    Pathfinding

---
+++ Assets/Scripts/Enemy_Scripts/EnemyMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+	Transform player;               // Reference to the player's position.
+	UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+
+	void Update ()
+	{
+		nav.SetDestination (player.position);
+	} 
+}
